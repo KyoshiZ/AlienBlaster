e250457ca4b492b8e2e34e53d78e36a1be95986c
KyoshiZ <adnantrgt27@gmail.com>
2024-07-10 14:42:53 +0300

    Added a maximum jump time to the player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private float _jumpEndTime;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +18,14 @@ public class Player : MonoBehaviour
         var horizontal = Input.GetAxis("Horizontal");
         Debug.Log(horizontal);
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector2(horizontal, rb.velocity.y);
+        var vertical = rb.velocity.y;
 
+        if (Input.GetButtonDown("Fire1"))
+            _jumpEndTime = Time.time + 0.5f;
+
+        if (Input.GetButton("Fire1") && _jumpEndTime > Time.time)
+            vertical = 5;
+
+        rb.velocity = new Vector2(horizontal, vertical);
     }
 }
