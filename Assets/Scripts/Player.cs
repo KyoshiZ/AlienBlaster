dbef46fab11c84cc2f21c1f4af3ce6ef2a4bbfa7
KyoshiZ <adnantrgt27@gmail.com>
2024-07-13 16:28:32 +0300

    Added the player layer assigned the player to it Swapped polygoncollider with capsule collider.Added layermask check to player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,7 @@ public class Player : MonoBehaviour
     [SerializeField] float _jumpVelocity = 5;
     [SerializeField] float _jumpDuration = 0.5f;
     [SerializeField] Sprite _jumpSprite;
+    [SerializeField] LayerMask _layerMask;
     public bool IsGraunded;
     SpriteRenderer _spriteRenderer;
     Sprite _defaultSprite;
@@ -35,7 +36,7 @@ public class Player : MonoBehaviour
     void Update()
     {
         Vector2 origin = new Vector2(transform.position.x, transform.position.y - _spriteRenderer.bounds.extents.y);
-        var hit = Physics2D.Raycast(origin, Vector2.down, 0.1f);
+        var hit = Physics2D.Raycast(origin, Vector2.down, 0.1f, _layerMask);
         if (hit.collider)
             IsGraunded = true;
         else
